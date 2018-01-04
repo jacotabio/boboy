da75311b808a81187358a8222c9cbbe4a65c3ee9
jacotabio <32643675+jacotabio@users.noreply.github.com>
2018-01-04 12:45:48 +0800

    asdasdasd

--- sng/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require 'library/config.php';
-	unset($_SESSION['usr_login']);
-	session_destroy();
-	header('location: index.php');
-	exit;
--- sng/modules/cpanel/order_details.php
+++
@@ -1,5 +0,0 @@
-<div id="orderinfo-ajax-content">
-  <div class="loading-div">
-    <svg class="spinner" stroke="#5677fc" width="50px" height="50px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg"><circle class="circle" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle></svg>
-  </div>
-</div>
