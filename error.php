742e20996e5c687f202da8055bbfd5bfff6b99b7
jacotabio <32643675+jacotabio@users.noreply.github.com>
2018-02-28 18:40:26 +0800

    created error page

--- error.php
+++ error.php
@@ -1,1 +1,72 @@
-Page Unavailable
+<!DOCTYPE html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="John Carlo H. Octabio">
+    <link rel="icon" href="favicon.ico">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/stylesheet.min.css" rel="stylesheet" type="text/css">
+    <link href="css/step-wizard.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap-theme.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+    <title>Boboy | 404 Error</title>
+    <!-- Bootstrap core CSS -->
+    
+    <link href="css/dataTables.material.min.css" rel="stylesheet">
+    <link href="css/material.min.css" rel="stylesheet">
+    <link href="css/bootstrap-switch.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/spinner.css" rel="stylesheet" type="text/css">
+    <link href="css/chat.css" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+    <nav id="nav-id" class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <div style="margin-left: 24px;">
+            <a class="navbar-brand" href="/?mod=cpanel" style="margin-top:0;padding-top:12px;"><img src="img/logo.png" height="25px"></a>
+          </div>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li class=><a href="/" class="uppercase">Home</a></li>
+            <li class=><a href="/?mod=shop" class="uppercase">Shop</a></li>
+
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+    <div style="margin-top:50px;width:100%;display:block;text-align: center;">
+    	<span style="display:block;margin-top:290px;padding-bottom:200px;text-align: center;">ERROR 404: Page Cannot Be Found</span>
+    </div>
+
+    <!-- Footer Content Goes Here -->
+    <footer id="myFooter">
+        <div class="container">
+
+        </div>
+        <div class="second-bar">
+           <div class="container">
+                <span style="width: 10px;">Boboy © 2017</span>
+                <div class="" style="margin:0;padding:0;">
+                    <a href="#" target="_blank" class="twitter"><i class="fa fa-twitter"></i></a>
+                    <a href="https://www.facebook.com/iamsleepnot/" target="_blank" class="facebook"><i class="fa fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/iamsleepnot/" target="_blank" class="instagram"><i class="fa fa-instagram"></i></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+  </body>
+</html>
--- script.sql
+++ script.sql
@@ -74,9 +74,6 @@ CREATE TABLE IF NOT EXISTS `conversations` (
 
 -- Dumping data for table db_boboy.conversations: ~1 rows (approximately)
 /*!40000 ALTER TABLE `conversations` DISABLE KEYS */;
-INSERT INTO `conversations` (`convo_id`, `usr_id`, `brand_id`, `created_at`) VALUES
-	(4, 1, 14, '2018-02-25 22:30:41'),
-	(5, 2, 14, '2018-02-26 18:14:45');
 /*!40000 ALTER TABLE `conversations` ENABLE KEYS */;
 
 -- Dumping structure for table db_boboy.fees
@@ -131,99 +128,10 @@ CREATE TABLE IF NOT EXISTS `messages` (
   `msg_open` int(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`msg_id`),
   KEY `convo_id` (`convo_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=90 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_boboy.messages: ~71 rows (approximately)
+-- Dumping data for table db_boboy.messages: ~88 rows (approximately)
 /*!40000 ALTER TABLE `messages` DISABLE KEYS */;
-INSERT INTO `messages` (`msg_id`, `convo_id`, `msg`, `sender_id`, `created_at`, `show_notif`, `msg_open`) VALUES
-	(19, 4, 'yow', 14, '2018-02-25 22:30:41', 1, 0),
-	(20, 4, 'asdasd', 14, '2018-02-25 22:41:50', 1, 0),
-	(21, 4, 'asd', 14, '2018-02-25 22:47:43', 1, 0),
-	(22, 4, 'asd', 14, '2018-02-25 22:47:45', 1, 0),
-	(23, 4, 'asd', 14, '2018-02-25 22:47:50', 1, 0),
-	(24, 4, 'asd', 14, '2018-02-25 22:47:51', 1, 0),
-	(25, 4, 'wassup', 14, '2018-02-25 22:47:53', 1, 0),
-	(26, 4, 'kamusta?', 14, '2018-02-25 22:48:02', 1, 0),
-	(27, 4, 'asd', 14, '2018-02-25 22:49:16', 1, 0),
-	(28, 4, 'asd', 1, '2018-02-25 22:50:30', 0, 0),
-	(29, 4, 'hahahaha', 1, '2018-02-25 22:51:26', 0, 0),
-	(30, 4, 'tol', 1, '2018-02-25 22:56:53', 0, 0),
-	(31, 4, 'op?', 14, '2018-02-25 22:56:59', 1, 0),
-	(32, 4, 'may customer ka da lihog assist', 1, '2018-02-25 22:57:13', 0, 0),
-	(33, 4, 'asd', 14, '2018-02-25 22:58:03', 1, 0),
-	(34, 4, 'asdasdasdasd', 1, '2018-02-25 23:00:13', 0, 0),
-	(35, 4, 'dddd', 1, '2018-02-25 23:00:17', 0, 0),
-	(36, 4, 'asd', 1, '2018-02-25 23:32:07', 0, 0),
-	(37, 4, 'asd', 1, '2018-02-25 23:32:30', 0, 0),
-	(38, 4, 'asd', 1, '2018-02-25 23:32:31', 0, 0),
-	(39, 4, 'asd', 1, '2018-02-25 23:32:31', 0, 0),
-	(40, 4, 'as', 1, '2018-02-25 23:32:31', 0, 0),
-	(41, 4, 'asd', 1, '2018-02-25 23:32:31', 0, 0),
-	(42, 4, 'SleepNot', 1, '2018-02-25 23:36:04', 0, 0),
-	(43, 4, 'Yes?', 14, '2018-02-26 18:03:00', 1, 0),
-	(44, 4, 'Do we have a problem here?', 14, '2018-02-26 18:07:12', 1, 0),
-	(45, 4, 'kjh', 14, '2018-02-26 18:07:30', 1, 0),
-	(46, 4, 'kj', 1, '2018-02-26 18:08:15', 0, 0),
-	(47, 4, 'jhg', 1, '2018-02-26 18:08:21', 0, 0),
-	(48, 4, 'okay2', 14, '2018-02-26 18:08:44', 1, 0),
-	(49, 4, 'asd', 14, '2018-02-26 18:12:13', 1, 0),
-	(50, 4, 'd', 14, '2018-02-26 18:12:15', 1, 0),
-	(51, 4, 'asd', 14, '2018-02-26 18:13:15', 1, 0),
-	(52, 5, 'sir', 14, '2018-02-26 18:14:45', 0, 0),
-	(53, 5, 'asd', 14, '2018-02-26 18:14:47', 0, 0),
-	(54, 5, 'asd', 14, '2018-02-26 18:14:49', 0, 0),
-	(55, 5, 'asd', 14, '2018-02-26 18:14:50', 0, 0),
-	(56, 5, 'asd', 14, '2018-02-26 18:14:51', 0, 0),
-	(57, 5, 'asd', 14, '2018-02-26 18:14:52', 0, 0),
-	(58, 5, 'asd', 14, '2018-02-26 18:14:54', 0, 0),
-	(59, 4, 'asd', 1, '2018-02-26 18:15:27', 0, 0),
-	(60, 4, 'asd', 14, '2018-02-26 18:15:37', 1, 0),
-	(61, 4, 'asd', 14, '2018-02-26 18:19:31', 1, 0),
-	(62, 4, 'd', 14, '2018-02-26 18:19:33', 1, 0),
-	(63, 4, 'asd', 1, '2018-02-26 18:19:59', 0, 0),
-	(64, 4, 'asd', 1, '2018-02-26 18:20:01', 0, 0),
-	(65, 4, 'asd', 14, '2018-02-26 18:21:15', 1, 0),
-	(66, 4, 'd', 14, '2018-02-26 18:21:16', 1, 0),
-	(67, 4, 'asd', 14, '2018-02-26 18:22:26', 1, 0),
-	(68, 4, 'asd', 14, '2018-02-26 18:25:34', 1, 0),
-	(69, 4, 'ddddd', 14, '2018-02-26 18:25:37', 1, 0),
-	(70, 4, 'd', 14, '2018-02-26 18:27:03', 1, 0),
-	(71, 4, 'asd', 14, '2018-02-26 18:27:24', 1, 0),
-	(72, 4, 'd', 14, '2018-02-26 18:27:28', 1, 0),
-	(73, 4, 'd', 14, '2018-02-26 18:27:31', 1, 0),
-	(74, 4, 'd', 14, '2018-02-26 18:28:56', 1, 0),
-	(75, 4, 'd', 14, '2018-02-26 18:29:34', 1, 0),
-	(76, 4, 'd', 14, '2018-02-26 18:31:51', 1, 0),
-	(77, 4, 'd', 14, '2018-02-26 18:35:05', 1, 0),
-	(78, 4, 'asdasd', 14, '2018-02-26 18:35:08', 1, 0),
-	(79, 4, 'asd', 14, '2018-02-26 18:37:15', 1, 0),
-	(80, 4, 'd', 14, '2018-02-26 18:37:29', 1, 0),
-	(81, 4, 'asd', 14, '2018-02-26 18:37:31', 1, 0),
-	(82, 4, 'asd', 1, '2018-02-26 19:36:21', 0, 0),
-	(83, 4, 'hoy', 1, '2018-02-28 16:37:19', 0, 0),
-	(84, 4, 'asd', 1, '2018-02-28 16:37:59', 0, 0),
-	(85, 4, 'yow', 1, '2018-02-28 16:39:36', 0, 0),
-	(86, 4, 'yes', 14, '2018-02-28 16:39:45', 1, 0),
-	(87, 4, 'waay lang', 1, '2018-02-28 16:39:48', 0, 0),
-	(88, 4, 'asd', 1, '2018-02-28 16:47:51', 0, 0),
-	(89, 5, 'yow', 2, '2018-02-28 17:53:02', 0, 0),
-	(90, 4, 'yes?', 1, '2018-02-28 18:08:26', 0, 0),
-	(91, 4, 'asd', 1, '2018-02-28 18:08:36', 0, 0),
-	(92, 4, 'asd', 1, '2018-02-28 18:08:37', 0, 0),
-	(93, 4, 'asd', 1, '2018-02-28 18:08:38', 0, 0),
-	(94, 4, 'asd', 1, '2018-02-28 18:08:38', 0, 0),
-	(95, 4, 'asd', 1, '2018-02-28 18:08:38', 0, 0),
-	(96, 4, 'asd', 1, '2018-02-28 18:08:38', 0, 0),
-	(97, 5, 'asd', 2, '2018-02-28 18:09:21', 0, 0),
-	(98, 4, 'd', 1, '2018-02-28 18:09:37', 0, 0),
-	(99, 4, 'wek wek wek', 1, '2018-02-28 18:09:46', 0, 0),
-	(100, 4, 'asd', 1, '2018-02-28 18:10:18', 0, 0),
-	(101, 4, 'asd', 1, '2018-02-28 18:10:29', 0, 0),
-	(102, 4, 'asd', 1, '2018-02-28 18:11:48', 0, 0),
-	(103, 4, 'asd', 1, '2018-02-28 18:19:28', 0, 0),
-	(104, 4, 'I am sleepnot', 14, '2018-02-28 18:22:20', 1, 0),
-	(105, 4, 'How may I help you?', 14, '2018-02-28 18:22:25', 1, 0),
-	(106, 4, 'Baw daw pihu ka hahaha', 1, '2018-02-28 18:23:40', 0, 0);
 /*!40000 ALTER TABLE `messages` ENABLE KEYS */;
 
 -- Dumping structure for table db_boboy.oitem
@@ -241,14 +149,8 @@ CREATE TABLE IF NOT EXISTS `oitem` (
   KEY `usr_id` (`usr_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=50000351 DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_boboy.oitem: 4 rows
+-- Dumping data for table db_boboy.oitem: 5 rows
 /*!40000 ALTER TABLE `oitem` DISABLE KEYS */;
-INSERT INTO `oitem` (`oi_id`, `order_id`, `item_id`, `oi_qty`, `oi_subtotal`, `usr_id`, `oi_status`, `oi_delivery`) VALUES
-	(50000348, 20000275, 17, 1, 45.00, 2, 1, 2),
-	(50000350, 20000277, 17, 1, 45.00, 2, 0, 0),
-	(50000349, 20000276, 17, 1, 45.00, 2, 0, 0),
-	(50000345, 20000273, 17, 1, 45.00, 2, 1, 2),
-	(50000344, 20000273, 20, 1, 70.00, 2, 1, 2);
 /*!40000 ALTER TABLE `oitem` ENABLE KEYS */;
 
 -- Dumping structure for table db_boboy.orders
@@ -265,13 +167,8 @@ CREATE TABLE IF NOT EXISTS `orders` (
   PRIMARY KEY (`order_id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=20000278 DEFAULT CHARSET=latin1;
 
--- Dumping data for table db_boboy.orders: 3 rows
+-- Dumping data for table db_boboy.orders: 4 rows
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` (`order_id`, `order_total`, `created_at`, `order_status`, `usr_id`, `delivery_address`, `contact_number`, `custom_fee`, `custom_name`) VALUES
-	(20000277, 45.00, '2018-02-28 18:09:16', 0, 2, '#22 Clayton Street, St. Francis Village, Brgy. Taculing, Bacolod City', '09437095891', 50.00, ''),
-	(20000273, 115.00, '2018-02-23 16:01:16', 4, 2, '#22 Clayton Street, St. Francis Village, Brgy. Taculing, Bacolod City', '09437095891', 50.00, ''),
-	(20000276, 45.00, '2018-02-28 17:51:17', 0, 2, '#22 Clayton Street, St. Francis Village, Brgy. Taculing, Bacolod City', '09437095891', 50.00, ''),
-	(20000275, 45.00, '2018-02-28 14:56:17', 4, 2, '#22 Clayton Street, St. Francis Village, Brgy. Taculing, Bacolod City', '09437095891', 50.00, '');
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 
 -- Dumping structure for table db_boboy.users
